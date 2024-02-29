e2349ecbd3d88eafcbcdd5bd7b6e1db7108a02d2
revue0813 <revue0813@gmail.com>
2024-02-28 18:11:12 -0600

    added / to end of routes, added dashboard route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -7,10 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
 
-@app.route('/test')
+@app.route('/test/')
 def about():
     return render_template('test.html')
+
+@app.route("/admin/")
+def admin():
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__": # If this app is not being called as part of a module, then:
