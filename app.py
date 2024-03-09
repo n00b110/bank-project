07603fd048b5f946fc70432e133091a634791099
revue0813 <revue0813@gmail.com>
2024-03-09 15:04:39 -0600

    implemented login logic using sessions

--- app.py
+++ app.py
@@ -1,31 +1,71 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+# Secret key for session encryption
+app.secret_key = "budgeting101"
+# Lifetime of the permenent session
+app.permanent_session_lifetime = timedelta(minutes=5)
 
-
-@app.route('/')
+@app.route('/', methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+    # Login
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["emailName"]
+        session["user"] = user
+        return redirect(url_for("dashboard"))
+    else:
+        if "user" in session:
+            return redirect(url_for('dashboard'))
+        return render_template("index.html")
 
 @app.route('/dashboard/')
 def dashboard():
-    return render_template("dashboard.html")
+    if "user" in session:
+        user = session["user"]
+        return render_template("dashboard.html", )
+    else:
+        return redirect(url_for("index"))
 
 @app.route('/budget/')
 def budget():
-    return render_template('budget.html')
+    if "user" in session:
+        user = session["user"]
+        return render_template("budget.html")
+    else:
+        return redirect(url_for("index"))
 
 @app.route('/goals/')
 def goals():
-    return render_template('goals.html')
+    if "user" in session:
+        user = session["user"]
+        return render_template('goals.html')
+    else:
+        return redirect(url_for("index"))
 
 @app.route('/settings/')
 def settings():
-    return render_template('settings.html')
+    if "user" in session:
+        user = session["user"]
+        return render_template('settings.html')
+    else:
+        return redirect(url_for("index"))
 
-@app.route('/test/')
-def about():
-    return render_template('test.html')
+# @app.route('/test/')
+# def test():
+#     if "user" in session:
+#         user = session["user"]
+#         return f"<h1>{user}</h1>"
+#     else:
+#         return redirect(url_for("index"))
+
+@app.route("/logout/")
+def logout():
+    if "user" in session:
+        user = session["user"]
+    session.pop("user", None)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__": # If this app is not being called as part of a module, then:
     app.run(debug=True)
